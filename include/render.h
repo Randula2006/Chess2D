beeec91e4e2ccccc3301cf6d49c1281ff8ef2f53
Randula Gunathilake <randula30085@gmail.com>
2026-04-21 08:42:12 +0530

    Create Textures for the Pieces and rendering logic

--- include/render.h
+++ include/render.h
@@ -6,7 +6,13 @@
 
 #define BLOCK_SIZE 100
 
+typedef struct{
+    SDL_Texture * pieces[2][6]; /* as 2 x 6 means 12 pieces as in chess*/
+} Textures;
+
+
 void render_board(SDL_Renderer* renderer, Board* board);
+SDL_Texture * load_pieces(SDl_Renderer * renderer);
 
 
 #endif
